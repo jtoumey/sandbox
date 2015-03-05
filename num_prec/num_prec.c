f10af609cd7f250a23f6c39f9441bf4b6a78dfcc
jtoumey <jtoumey@gmail.com>
2015-03-04 23:14:01 -0600

    fixed the bugs that result when you don't touch C code for 3 years

--- num_prec/num_prec.c
+++ num_prec/num_prec.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 
+double f(double);
 
-int main()
+int main(void)
 {
-   /* My first proram */
-   printf("Hello, World! \n");
-
+   // Variable declarations
+   double x;
+   double y;
+   
+   x = 10.;
+   y = f(x);
+   printf("Function f is: %d",y);
    return 0;
+}
+
+double f(double x)
+{
+   // Variable declarations
+   double y;
+   
+   // code
+   y = log(1 + exp(x));
+
+   return(y);
 }
