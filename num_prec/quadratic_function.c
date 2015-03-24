a941495bde5fef47454b26f6b67f673c0572ea25
Julian T <jtoumey@gmail.com>
2015-03-23 22:33:35 -0500

    variable declarations

--- num_prec/quadratic_function.c
+++ num_prec/quadratic_function.c
@@ -24,12 +24,31 @@
 #include <math.h>
 #include <float.h>
 
+double root_naive(double);
+
 int main(void)
 {
+   //
+   double a = 3.0, b = 1.0e9, c = 5.0;
+    
    //
    printf("Calculate the roots of: 3 * x^2 + 10^9 * x + 5 = 0\n");
    
    
    
    return(0);
+}
+
+
+/******************************************************************************
+* Function:    root_naive
+* Description: naively calculates the roots of a quadratic equation using a 
+*              formula that fail due to limited floating point precision. 
+* Parameters:  , data type, and description>
+*              <variable2 name, data type, and description>
+* Return:      <data type and description>
+******************************************************************************/
+double root_naive(double a,double b, double c)
+{
+   
 }
