0f5cf9e4f24be7f58db6b624f42b324b5cd43aea
Julian T <jtoumey@gmail.com>
2015-03-23 22:00:46 -0500

    added function to handle overflow and large negative numbers (float.h)

--- num_prec/num_prec.c
+++ num_prec/num_prec.c
@@ -21,29 +21,29 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <float.h>
 
 double f(double);
-double f_over(double);
+double f_check(double);
 double get_input();
 
 int main(void)
 {
    // Variable declarations
    double x,y;
-   double y_over;
-   double y_under;
+   double y_check;
    
    // Collect input
    x = get_input();
 
    // Calculate the result
    y = f(x);
-   y_over = f_over(x);
+   y_check = f_check(x);
 
    // Print results
 
-   printf("Function f is     : %f\n",y);
-   printf("Function f_over is: %f\n",y_over);
+   printf("Function f is      : %f\n",y);
+   printf("Function f_check is: %f\n",y_check);
    return 0;
 }
 
@@ -81,7 +81,11 @@ double f(double x)
                      Solution: if x is large enough to overflow the EXP operation, then
                                just return x.
                             
-         Negative x: 
+         Negative x: returns a value so small that the computer cannot distinguish 1
+                     from 1 + exp(-50). The function returns log(1+0) = 0.
+                     
+                     Solution: If the computer thinks 1+x is essentially 1, then we can
+                     return exp(x).
    */
       
    // code
@@ -90,20 +94,15 @@ double f(double x)
    return(y);
 }
 
-double f_over(double x)
+double f_check(double x)
 {
    // Variable declarations
-   double y,X_MAX;
-   X_MAX = 1000.;
+   double X_MIN,X_MAX;
+   X_MAX = -log(DBL_EPSILON);
+   X_MIN =  log(DBL_EPSILON);
 
    // calculation
-   if(x > X_MAX)
-   {
-      y = x;
-   }
-   else
-   {
-      y = log(1 + exp(x));
-   }
-   return(y);
+   if(x > X_MAX) return x;
+   if(x < X_MIN) return exp(x);
+   return log(1.0 + exp(x));
 }
